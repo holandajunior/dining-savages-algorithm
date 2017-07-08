5929865e5a0fec16d1de2534fecc938d405325ab
Holanda Junior <joaoholandajr@gmail.com>
2017-07-08 12:05:25 -0300

    Finishing cook thread

--- src/com/ufc/InfoUtils.java
+++
@@ -1,6 +0,0 @@
-package com.ufc;
-
-public class InfoUtils {
-
-    public static final int NUMBER_SAVAGES = 3;
-}
--- src/com/ufc/semaphore/Cook.java
+++ src/com/ufc/semaphore/Cook.java
@@ -1,11 +1,45 @@
 package com.ufc.semaphore;
 
 
+import java.util.concurrent.Semaphore;
+
 public class Cook implements Runnable {
+
+    private static final String ENTITY = "Cook";
+
+    private DiningSavagesSemaphore coordinator;
+
+    private Semaphore mutex;
+    private Semaphore emptyPot;
+    private Semaphore fullPot;
+
+    public Cook( DiningSavagesSemaphore coordinator,
+                 Semaphore mutex,
+                 Semaphore emptyPot,
+                 Semaphore fullPot ) {
+
+        this.coordinator = coordinator;
+
+        this.mutex = mutex;
+        this.emptyPot = emptyPot;
+        this.fullPot = fullPot;
+    }
 
     @Override
     public void run() {
 
+
+        while( true ) {
+
+            emptyPot.acquireUninterruptibly();
+
+            System.out.println( ENTITY + " is refilling pot..." );
+            coordinator.refillPot();
+            System.out.println( ENTITY + " refilled pot..." );
+
+            fullPot.release();
+
+        }
 
 
     }
