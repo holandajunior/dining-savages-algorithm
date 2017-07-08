622a4e31483a16b38e0be48d8074efd21049bab4
Holanda Junior <joaoholandajr@gmail.com>
2017-07-08 12:06:16 -0300

    Finishing algorith flow controller

--- src/com/ufc/semaphore/DiningSavagesSemaphore.java
+++ src/com/ufc/semaphore/DiningSavagesSemaphore.java
@@ -1,53 +1,84 @@
 package com.ufc.semaphore;
 
-import com.ufc.InfoUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class DiningSavagesSemaphore {
 
-    private static final int NUMBER_SAVAGES = InfoUtils.NUMBER_SAVAGES;
-
     private Semaphore mutex;
+    private Semaphore emptyPot;
+    private Semaphore fullPot;
+
+    private final int potCapacity;
     private int stewedInPot;
 
-    public DiningSavagesSemaphore( int stewedInPot ) {
+    public DiningSavagesSemaphore( int potCapacity ) {
 
-        this.stewedInPot = stewedInPot;
         mutex = new Semaphore( 1 );
+        emptyPot = new Semaphore(0);
+        fullPot = new Semaphore(0);
+
+        this.potCapacity = potCapacity;
+        this.stewedInPot = potCapacity;
     }
 
-    public int getFood() {
-
-        stewedInPot -= 1;
+    public int getStewedInPot() {
         return stewedInPot;
+    }
+
+    public void eat() {
+        stewedInPot -= 1;
+    }
+
+    public boolean refillPot() {
+
+        stewedInPot = potCapacity;
+        return true;
     }
 
     public Semaphore getMutex() {
         return mutex;
     }
 
+    public Semaphore getEmptyPot() {
+        return emptyPot;
+    }
 
+    public Semaphore getFullPot() {
+        return fullPot;
+    }
 
+    /**
+     *
+     * @param args [0] -> Number of savage threads; [1] -> Pot capacity
+     */
     public static void main(String[] args) {
 
-        DiningSavagesSemaphore coordinator = new DiningSavagesSemaphore( 5 );
+        int numberSavages = Integer.valueOf( args[0] );
+        int potCapacity = Integer.valueOf( args[1] );
+
+        DiningSavagesSemaphore coordinator = new DiningSavagesSemaphore( potCapacity );
 
         // Create all savage threads
-        ExecutorService savagePool = Executors.newFixedThreadPool( DiningSavagesSemaphore.NUMBER_SAVAGES );
-        for( int savage = 0; savage < NUMBER_SAVAGES; savage++ ) {
+        ExecutorService savagePool = Executors.newFixedThreadPool( numberSavages );
+        for( int savage = 0; savage < numberSavages; savage++ ) {
 
-            savagePool.submit( new Savage( coordinator.getMutex() ) );
+            savagePool.submit( new Savage( savage,
+                                           coordinator,
+                                           coordinator.getMutex(),
+                                           coordinator.getEmptyPot(),
+                                           coordinator.getFullPot() ) );
 
         }
 
+
         //Create cook thread
         ExecutorService cookPool = Executors.newSingleThreadExecutor();
-        cookPool.submit( new Cook() );
+        cookPool.submit( new Cook( coordinator,
+                                   coordinator.getMutex(),
+                                   coordinator.getEmptyPot(),
+                                   coordinator.getFullPot() ) );
 
     }
 }
