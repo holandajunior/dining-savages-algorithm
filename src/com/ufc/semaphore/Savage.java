d57c07c1a76ab3b35eaf7364a5e6a1461413a964
Holanda Junior <joaoholandajr@gmail.com>
2017-07-08 12:05:40 -0300

    Finishing savage thread

--- src/com/ufc/semaphore/Savage.java
+++ src/com/ufc/semaphore/Savage.java
@@ -4,18 +4,52 @@ import java.util.concurrent.Semaphore;
 
 public class Savage implements Runnable {
 
-    private Semaphore mutex;
+    private static final String ENTITY = "Savage";
 
-    public Savage( Semaphore mutex ) {
+    private DiningSavagesSemaphore coordinator;
+    private int myNumber;
+
+    private Semaphore mutex;
+    private Semaphore emptyPot;
+    private Semaphore fullPot;
+
+    public Savage( int myNumber,
+                   DiningSavagesSemaphore coordinator,
+                   Semaphore mutex,
+                   Semaphore emptyPot,
+                   Semaphore fullPot ) {
+
+        this.coordinator = coordinator;
+        this.myNumber = myNumber;
+
         this.mutex = mutex;
+        this.emptyPot = emptyPot;
+        this.fullPot = fullPot;
     }
 
     @Override
     public void run() {
 
-        while( true ) {
+        try {
 
+            while (true) {
+
+                Thread.sleep(2 * 1000);
+                mutex.acquireUninterruptibly();
+                if (coordinator.getStewedInPot() == 0) {
+
+                    emptyPot.release();
+                    fullPot.acquireUninterruptibly();
+                }
+
+                System.out.println(ENTITY + " " + myNumber + " is eating food " + coordinator.getStewedInPot() + "...");
+                coordinator.eat();
+
+                mutex.release();
+            }
+
+        } catch( InterruptedException e ) {
+            e.printStackTrace();
         }
-
     }
 }
